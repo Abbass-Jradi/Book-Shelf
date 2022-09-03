4ac6292ff8ea8ff911e87cbb60ade3107b8e2391
Abbass_Jradi <jradiabbass@gmail.com>
2022-09-03 08:57:39 +0300

    Add basic Book Constructor

---
+++ main.js
@@ -0,0 +1,8 @@
+let myLibrary = [];
+
+function Book(author, title, numberOfPages, isRead){
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+    this.isRead = isRead;
+}
